647c020de7de5446c4796c7c0a86faa5bc64a3f4
bgmcmichael <fenjifoxtail@gmail.com>
2016-10-25 18:10:49 -0400

    initial commit

---
+++ src/main/java/com/lifx/LightControllerApplication.java
@@ -0,0 +1,12 @@
+package com.lifx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightControllerApplication.class, args);
+	}
+}
